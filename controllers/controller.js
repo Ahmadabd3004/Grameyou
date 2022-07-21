5fac988cabbfba04f40baded15b002a0801daa15
Ahmadabd3004 <ahmadazis3004@gmail.com>
2022-07-21 20:08:53 +0700

    add regis and login

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,45 +1,32 @@
 const { Book, User, Genre } = require('../models/')
 const { Op } = require("sequelize");
-
+const bcrypt = require('bcryptjs');
 
 class Controller {
-    static home(req,res) {
+    static home(req, res) {
         res.render('home')
     }
 
-    static register(req, res){
-        res.render('registerForm')
-    }
 
-    static postRegister(req,res){
-        const {email, password, role} = req.body
-        User.create({email, password, role})
-        .then(()=> {
-            res.redirect('registerForm')
-        })
-        .catch(err => {
-            res.send(err)
-        })
-    }
-    
-    static bookList(req,res) {
+    static bookList(req, res) {
 
         Book.findAll()
-        .then(book => {
-            res.render('bookList', {book})
-        })
-        .catch(err =>{
-            res.send(err)
-        })
-   
+            .then(book => {
+                res.render('bookList', { book })
+            })
+            .catch(err => {
+                res.send(err)
+            })
+
     }
-    static genre(req,res) {}
+    static genre(req, res) { }
 
     static home(req, res) {
+        let user = req.session.user
         // res.send('masuk')
         Genre.findAll()
             .then(result => {
-                res.render('home', { result })
+                res.render('home', { result, user })
             })
             .catch(err => {
                 res.send(err)
@@ -49,11 +36,11 @@ class Controller {
     static bookList(req, res) {
         let search = req.query.search
         let query = {
-            order : [['title', 'ASC']],
+            order: [['title', 'ASC']],
         }
         if (search) {
             query.where = {
-                title : {
+                title: {
                     [Op.iLike]: `%${search}%`
                 }
             }
@@ -74,7 +61,7 @@ class Controller {
             include: Book
         }
         if (id) {
-            query.where = {id}
+            query.where = { id }
         }
         // res.send(id)
         Genre.findAll(query)
@@ -89,19 +76,19 @@ class Controller {
 
     static readBook(req, res) {
         let id = req.params.id
-        Book.findOne({where: {id}})
+        Book.findOne({ where: { id } })
             .then(books => {
                 // res.send(books)
-                res.render('bookDetail', {books})
+                res.render('bookDetail', { books })
             })
-            
+
     }
 
     static saveBook(req, res) {
         const { title, author, genre } = req.body
         Book.create({ title, author, genre })
             .then(() => {
-                
+
                 res.redirect('myBooks')
             })
             .catch(err => {
@@ -128,6 +115,54 @@ class Controller {
 
     static readersList(req, res) {
         res.redirect('readersList')
+    }
+
+    static register(req, res) {
+        res.render('registerForm')
+    }
+
+    static postRegister(req, res) {
+        // res.send(req.body)
+        let { username, password, role } = req.body
+        User.create({ username, password, role })
+            .then(() => {
+                res.redirect('/login')
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
+    static loginForm(req, res) {
+        let error = req.query.err
+        res.render('loginForm', { error })
+    }
+    static postLogin(req, res) {
+        let { username, password } = req.body
+        User.findOne({ where: { username } })
+            .then(user => {
+                if (user) {
+                    const isValid = bcrypt.compareSync(password, user.password);
+                    if (isValid) {
+                        req.session.user = user
+                        console.log(req.session)
+                        res.redirect('/')
+                    } else {
+                        const err = 'Password not found'
+                        res.redirect(`/login?err=${err}`)
+                    }
+                } else {
+                    const err = 'Username not found'
+                    res.redirect(`/login?err=${err}`)
+                }
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
+
+    static logout(req,res){
+        req.session.user = {}
+        res.redirect('/login')
     }
 }
 
--- models/membership.js
+++ models/membership.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Membership.hasOne(models.User)
+      Membership.belongsTo(models.User)
     }
   }
   Membership.init({
